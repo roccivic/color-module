4ee1495a80754e0ec610923733ceca7e34b3fd50
Rouslan Placella <rouslan@placella.com>
2015-04-11 18:14:35 +0100

    Split alpha tests

--- test/spec/alpha-tests.js
+++ test/spec/alpha-tests.js
@@ -1,21 +1,36 @@
-describe("Alpha test", function () {
-    it("getAlpha", function () {
-        var color = new Color();
-        expect(color.getAlpha()).toBe(1);
-
-        color.setColor('#fff0');
-        expect(color.getAlpha()).toBe(0);  
-
-        color.setColor({ h:0, s:0, l:0, a:0.5 });
-        expect(color.getAlpha()).toBe(0.5);   
+describe("Alpha test:", function () {
+    describe("getAlpha:", function () {
+        it("defaults to 1", function () {
+            var color = new Color();
+            expect(color.getAlpha()).toBe(1);
+        });
+        it("can be set to 0", function () {
+            var color = new Color();
+            color.setColor('#fff0');
+            expect(color.getAlpha()).toBe(0);  
+        });
+        it("can be set to 0.5", function () {
+            var color = new Color();
+            color.setColor({ h:0, s:0, l:0, a:0.5 });
+            expect(color.getAlpha()).toBe(0.5);
+        });
     });
-    it("setAlpha", function () {
-        var color = new Color();
-        color.setAlpha(1);
-        expect(color.getAlpha()).toBe(1);
-        color.setAlpha(0.5);
-        expect(color.getAlpha()).toBe(0.5);
-        color.setAlpha(5.7);
-        expect(color.getAlpha()).toBe(0.5);
+    describe("setAlpha:", function () {
+        it("can be set to 1", function () {
+            var color = new Color();
+            color.setAlpha(1);
+            expect(color.getAlpha()).toBe(1);
+        });
+        it("can be set to 0.5", function () {
+            var color = new Color();
+            color.setAlpha(0.5);
+            expect(color.getAlpha()).toBe(0.5);
+        });
+        it("cannot be set to 5.7", function () {
+            var color = new Color();
+            color.setAlpha(0.5);
+            color.setAlpha(5.7);
+            expect(color.getAlpha()).toBe(0.5);
+        });
     });
 });
